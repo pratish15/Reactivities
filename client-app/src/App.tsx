406061cc618afdc69688b1cf92257d7b0dd35c30
Pratish Prabhash (BPprabhashH AU) <pprabhash@bhworldwide.com>
2022-07-07 23:29:27 +1000

    End sec 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,29 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { ducks } from './demo';
 import DuckItem from './DuckItem';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
+// ducks.map(duck => (
+//   <DuckItem duck={duck}  key={duck.name} />
+// ))
 function App() {
+
+  const [activities, setActivities] = useState([]) // USE STATE
+
+  
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      console.log(response);
+      setActivities(response.data);
+    })
+  }, []) // [] ensures does not loop. only exuctes once
   return (
-    <div className="App">
+    <div >
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {
-        ducks.map(duck => (
-          <DuckItem duck={duck}  key={duck.name} />
-        ))}
-        <p style={{color:'red'}}>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header as='h2' icon='users' content='Reactivities' />
+        <List>
+          {activities.map((activity:any) =>(
+              <List.Item key ={activity.id}>
+                  {activity.title}
+              </List.Item>
+            ))}
+        </List>
+   
+
+
+
+
+
+
       </header>
     </div>
   );
