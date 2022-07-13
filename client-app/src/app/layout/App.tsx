829805dd7666e671f2d29e54c4cc1359d7266b15
Pratish Prabhash <pratishprabhash@gmail.com>
2022-07-13 14:05:32 +1000

    End of sec 6

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,13 +1,14 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import { ducks } from '../../demo';
 import DuckItem from '../../DuckItem';
-import axios from 'axios';
+import agent from '../api/agent';
 import { Container } from 'semantic-ui-react';
 
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { Activity } from '../models/activity';
 import {v4 as uuid} from 'uuid';
+import LoadingComponent from './LoadingComponents';
 // ducks.map(duck => (
 //   <DuckItem duck={duck}  key={duck.name} />
 // ))
@@ -16,9 +17,18 @@ function App() {
   const [activities, setActivities] = useState<Activity[]>([]) // USE STATE
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
+  const [loading, stateLoading] = useState(true);
+  const[submitting, setSubmitting] = useState(false);
+
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
-      setActivities(response.data);
+    agent.Activities.list().then(response => {
+      let activities: Activity[] =[];
+      response.forEach(activity => {
+        activity.date = activity.date.split('T')[0];
+        activities.push(activity);
+      })
+      setActivities(activities);
+      stateLoading(false);
     })
   }, []) // [] ensures does not loop. only exuctes once
 
@@ -41,18 +51,35 @@ function App() {
   }
 
   function handleCreateOrEditActivity(activity: Activity){
-
-    // Check if activity id then it will be in Edit mode else in create mode
-    activity.id? 
-    setActivities([...activities.filter(x=>x.id!=activity.id), activity])
-    : setActivities([...activities, {...activity, id:uuid()}]); // create activity
-    setEditMode(false);
-    setSelectedActivity(activity);
+    setSubmitting(true);
+     // Check if activity id then it will be in Edit mode else in create mode
+    if(activity.id){
+      agent.Activities.update(activity).then(() =>{
+      setActivities([...activities.filter(x=>x.id!=activity.id), activity]);
+      setSelectedActivity(activity);
+      setEditMode(false);
+      setSubmitting(false);
+      })
+    }else{
+      activity.id = uuid();
+      agent.Activities.create(activity).then(() =>{
+        setActivities([...activities, activity]); // create activity
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      })
+    }
   }
 
   function handleDeleteActivity(id:string){
+    setSubmitting(true);
+  agent.Activities.delete(id).then(()=>{
     setActivities([...activities.filter(x=>x.id !==id)])
+    setSubmitting(false);
+  })
+   
   }
+if(loading) return <LoadingComponent content='Loading app'/>
 
   return (
     <Fragment>
@@ -68,6 +95,7 @@ function App() {
           closeForm={handleFormClose}
           createOrEdit={handleCreateOrEditActivity}
           deleteActivity={handleDeleteActivity}
+          submitting={submitting}
           />
         </Container>
        
